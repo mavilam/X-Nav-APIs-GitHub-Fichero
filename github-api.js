efad0737017ce2b845c413488fd51484e1f649d6
Miguel Ávila Monroy <soyer893@gmail.com>
2015-04-22 23:39:22 +0200

    Subida de ejercicio

---
+++ github-api.js
@@ -0,0 +1,58 @@
+var repo;
+var ghObj;
+
+var newForm = "<input type='text'id='user' size='10' />" +
+    "<input type='text' id='repo' size='10' />" +
+    "<button type='button' id='dataButton'>Write Data</button>" ;
+
+
+function readToken(){
+	$("#newForm").html("");
+	var token = $("#token").val();
+	
+	ghObj = new Github({
+		token: token,
+		auth: "oauth"
+    });
+
+	$("#newForm").append(newForm);
+	$("#dataButton").click(getData);
+};
+
+function getData(){
+	console.log($("user").val());
+	console.log($("repo").val());
+	repo = ghObj.getRepo($("#user").val(), $("#repo").val());
+	repo.show(function(error,repo){
+		if (error) {
+			$("#newForm").append("<h3>Error: " + error.error + "</h3>");
+	    } else {
+			$("#newForm").append("<p>Repo data:</p>" +
+				      "<ul><li>Full name: " + repo.full_name + "</li>" +
+				      "<li>Description: " + repo.description + "</li>" +
+				      "<li>Created at: " + repo.created_at + "</li>" +
+				      "</ul><button id='write'>Write File</button>" +
+				      "<input type='text'id='text' size='10' /> " +
+				      "<div id='writefile' />");
+			
+			$("#write").click(writeRepo);
+	    }
+	});
+    
+};
+
+function writeRepo() {
+    repo.write('master', 'datafile', 
+		 $("#text").val(),
+		 "Appending Text", function(err) {
+		     console.log (err)
+		 });
+    	$("#newForm").append("<h2>Success!!</h2>");
+};
+
+$(document).ready(function() {
+
+    $("#formButton").click(readToken);
+
+    
+});
